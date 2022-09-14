d59fa4486e4da3ba3306808e8e38f4e71ded7218
DevSenpai <devsenpai09@gmail.com>
2022-09-14 12:44:58 +0100

    created boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
+import styled from "styled-components";
+
+const boxes = [
+  { color: "red", left: 0 },
+  { color: "blue", left: 20 },
+  { color: "green", left: 40 },
+  { color: "yellow", left: 60 },
+  { color: "purple", left: 80 },
+];
+
+const StyledCardWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 20rem);
+  gap: 3rem;
+  overflow: hidden;
+  margin-top: 3rem;
+`;
+
+const StyledCard = styled.div`
+  height: 20rem;
+  border-radius: 1rem;
+  grid-area: -1 / 1;
+  position: relative;
+`;
+
 const App = () => {
   return (
-    <div className="">
+    <div>
       <h1>React Infinite Slider</h1>
+      <StyledCardWrapper>
+        {boxes.map((box) => (
+          <StyledCard
+            key={boxes.indexOf(box)}
+            style={{
+              backgroundColor: box.color,
+              left: box.left + boxes.indexOf(box) * 3 + "rem",
+            }}
+          ></StyledCard>
+        ))}
+      </StyledCardWrapper>
     </div>
   );
 };
